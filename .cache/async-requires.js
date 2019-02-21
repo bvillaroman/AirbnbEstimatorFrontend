cba29c91391cab8e9ce2d7c66d975d6921261455
lihaojin <hli002@citymail.cuny.edu>
2019-02-21 11:28:47 -0500

    Initial Setup

---
+++ .cache/async-requires.js
@@ -0,0 +1,10 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("/Users/candicezhong/Documents/Github/AirbnbEstimatorFrontend/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-index-js": () => import("/Users/candicezhong/Documents/Github/AirbnbEstimatorFrontend/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
+}
+
+exports.data = () => import(/* webpackChunkName: "pages-manifest" */ "/Users/candicezhong/Documents/Github/AirbnbEstimatorFrontend/.cache/data.json")
+
