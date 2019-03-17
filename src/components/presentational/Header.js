299d60a03682ab3917d0c9aeaff6f9a79b12c3e6
Bruce <bvillaroman@gmail.com>
2019-03-17 18:25:26 -0400

    Updates

--- src/components/presentational/Header.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import HeaderStyles from "../../styles/header.module.css"
-
-export default ({children}) => (
-  <div className={HeaderStyles.headerContainer}>
-    Airbnb Price Estimator
-  </div>
-)
