c5e7cb8eeb9364e74450d40e66a0793fd1e299ca
Bruce <bvillaroman@gmail.com>
2019-03-17 18:19:29 -0400

    added input field for address

---
+++ src/components/presentational/Header.js
@@ -0,0 +1,8 @@
+import React from "react"
+import HeaderStyles from "../../styles/header.module.css"
+
+export default ({children}) => (
+  <div className={HeaderStyles.headerContainer}>
+    Airbnb Price Estimator
+  </div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,32 @@
 import React from "react"
-import Container from "../components/container/container"
+import Layout from "../components/presentational/layout"
+import Input from "../components/presentational/Input"
+import ContainerStyles from "../styles/layout.module.css"
 
-export default () => (
-  <Container>
-  </Container>
-)
+class App extends React.Component {
+  state = {
+    address:""
+  }
+
+  handleInput = (evt) => {
+    const { value,name } = evt.target;
+    this.setState({ [name] : value})
+  }
+
+  onSubmit = (evt) => {
+
+  }
+
+  render(){
+    return (
+      <Layout>
+        <div className={ContainerStyles.mainContainer}>
+          <Input label="Address" name={"address"} value={this.state.address} handleInput={this.handleInput}/>
+          <button onClick={this.onSubmit}> Submit </button>
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export default App;
