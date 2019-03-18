96f3fe3e68c6a6c13e0de0efa0e8dbad12978f43
Bruce <bvillaroman@gmail.com>
2019-03-17 22:31:59 -0400

    added axios call to submit to api

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import React from "react"
 import Layout from "../components/presentational/layout"
 import Input from "../components/presentational/Input"
 import ContainerStyles from "../styles/layout.module.css"
+import {sendListing} from "../utils/Listing"
 
 class App extends React.Component {
   state = {
-    address:""
+    address:"",
+    responseText:""
   }
 
   handleInput = (evt) => {
@@ -15,6 +17,19 @@ class App extends React.Component {
 
   onSubmit = (evt) => {
 
+    const data = {
+      location: {
+        address: this.state.address
+      }
+    }
+    
+    sendListing(`${process.env.API_URL}/listings`,data)
+      .then((res) => {
+        this.setState({responseText: "Succesfully submitted"})
+      })
+      .catch(err => {
+        this.setState({responseText: "Something went wrong"})
+      })
   }
 
   render(){
@@ -23,6 +38,9 @@ class App extends React.Component {
         <div className={ContainerStyles.mainContainer}>
           <Input label="Address" name={"address"} value={this.state.address} handleInput={this.handleInput}/>
           <button onClick={this.onSubmit}> Submit </button>
+          <br/>
+          <br/>
+          <div>{this.state.responseText}</div>
         </div>
       </Layout>
     )
---
+++ src/utils/Listing.js
@@ -0,0 +1,18 @@
+
+import Axios from "Axios"
+
+export function sendListing(url,listingData){
+  const options = {
+    method: 'post',
+    url: url,
+    data: JSON.stringify(listingData)
+  }
+
+  return Axios(options)
+    .then((res) => {
+      return res.data
+    })
+    .catch((err) => {
+      return err;
+    });
+}
