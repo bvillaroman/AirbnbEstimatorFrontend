dc346220b3705c1a4b70b5918c62f01e578e4923
Bruce <bvillaroman@gmail.com>
2019-03-20 16:07:28 -0400

    added more inputs /edited the POST request to serverless

--- src/components/presentational/Input.js
+++ src/components/presentational/Input.js
@@ -3,7 +3,7 @@ import ContainerStyles from "../../styles/component.module.css"
 
 export default ({label,value,name,handleInput}) => (
   <div className={ContainerStyles.inputContainer}>
-    <div className={ContainerStyles.label}>{label}: </div>
-    <input name={name} value={value} placeholder={`Input ${label}`} onChange={handleInput}/>
+    <div className={ContainerStyles.label}>{label} </div>
+    <input name={name} value={value} onChange={handleInput}/>
   </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,10 @@ import {sendListing} from "../utils/Listing"
 class App extends React.Component {
   state = {
     address:"",
+    accommodates:0, 
+    beds: 0, 
+    bedrooms: 0, 
+    bathrooms: 0,
     responseText:""
   }
 
@@ -16,11 +20,18 @@ class App extends React.Component {
   }
 
   onSubmit = (evt) => {
+    const { address, accommodates, beds, bedrooms, bathrooms } = this.state;
 
     const data = {
       location: {
-        address: this.state.address
-      }
+        address: address,
+      },
+      bedrooms: {
+        guests: accommodates,
+        numOfBedrooms: bedrooms,
+        numOfBedsAvailable: beds
+      },
+      bathrooms: bathrooms 
     }
     
     sendListing(`${process.env.API_URL}/listings`,data)
@@ -36,7 +47,11 @@ class App extends React.Component {
     return (
       <Layout>
         <div className={ContainerStyles.mainContainer}>
-          <Input label="Address" name={"address"} value={this.state.address} handleInput={this.handleInput}/>
+          <Input label="What is the Address?" name={"address"} value={this.state.address} handleInput={this.handleInput}/>
+          <Input label="How many guests?" name={"accommodates"} value={this.state.accommodates} handleInput={this.handleInput}/>
+          <Input label="How many beds?" name={"beds"} value={this.state.beds} handleInput={this.handleInput}/>
+          <Input label="How many bedrooms?" name={"bedrooms"} value={this.state.bedrooms} handleInput={this.handleInput}/>
+          <Input label="How many bathrooms?" name={"bathrooms"} value={this.state.bathrooms} handleInput={this.handleInput}/>
           <button onClick={this.onSubmit}> Submit </button>
           <br/>
           <br/>
